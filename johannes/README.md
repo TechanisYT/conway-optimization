8e5f25019761652acd7ab868366a50aa2e600808
Techanis <johannesmelcher6@gmail.com>
2023-03-16 09:08:17 +0100

    manu

---
+++ johannes/README.md
@@ -0,0 +1,19 @@
+# conway-optimization
+optimizing a given program for the commodore c64
+
+Changes | Time [s]	| FPS
+-------- | -------- | ---------
+Stock   | 56.2	| 0.0
+print commented out | 42.0 | 0.0
+temp not needed | 40.9 | 0.0
+redundant ifs pruefeRegeln | 38.1 | 0.0
+New PruefeRegeln Call | 38.7 | 0.0
+redundant flag in zaehlLebende | 36 | 0.0
+new zaehllebende, without FindNachbarn | 19.8 | 0.0
+all functions new | 4.9 | 0.1
+bug-fixing | 3.6 | 0.2
+null array, compiler options, unsigned char | 1.7 | 0.5
+zeros | 1.4 | 0.6
+
+
+https://cc65.github.io/doc/funcref.html
---
+++ johannes/conway.c
@@ -0,0 +1,208 @@
+#include <c64.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <stdio.h>
+
+#define P_SCREEN0 ((unsigned char *)0x2000)
+#define P_COLOR0  ((unsigned char *)0x0400)
+
+#define P_SCREEN1 ((unsigned char *)0x6000)
+#define P_COLOR1  ((unsigned char *)0x4400)
+
+//HighRes Mode 300x200px
+#define XMAX 40
+#define YMAX 25
+#define ROUNDS 100
+
+//static const char array[XMAX][YMAX] 
+unsigned char array[XMAX][YMAX]= {
+{0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,1},
+{1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,1,1,0,0,0,0,0,0,1,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1},
+{0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+{1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0},
+};
+
+unsigned char null[XMAX][YMAX];
+unsigned char temp[XMAX][YMAX];
+unsigned char lebende[XMAX][YMAX];
+
+unsigned char main(void)
+{
+
+	unsigned char background = bgcolor(COLOR_BLACK);
+	unsigned char text = textcolor(COLOR_WHITE);
+
+	unsigned long sec;
+  unsigned char sec10;
+  unsigned long fps;
+  unsigned char fps10;
+        
+	unsigned char x;
+	unsigned char y;
+
+	unsigned char round = 0;
+  clock_t       t;
+  t = clock ();
+
+	//while(round < ROUNDS && !kbhit()){
+  do {
+  	memcpy(temp,null,XMAX*YMAX);
+  	memcpy(lebende,null,XMAX*YMAX);
+		
+		//for(y = 0; y< YMAX; y++){
+		y=0;
+		do {
+			x=0;
+			//for(x = 0; x< XMAX; x++){
+			do {
+				if (array[x][y] == 1)
+				{
+					char xi,yi = 0,minx = 0,maxx = 2,maxy = 2;
+					switch (x)
+					{
+					 	case 0:
+					 		minx=1;
+					 		break;
+					 	case XMAX-1:
+					 		maxx=1;
+					 		break;
+					}
+					switch (y)
+					{
+						case 0:
+							yi=1;
+					 		break;
+						case YMAX-1:
+					 		maxy=1;
+					 		break;
+				  }
+					//for(yi = miny; yi <= maxy; yi++){
+				  do {
+				  	xi=minx;
+						//for(xi = minx; xi <= maxx; xi++){
+				  	do {
+								lebende[x+xi-1][y+yi-1] += 1;
+								xi++;
+						}while(xi<=maxx);
+						yi++;
+					}while(yi<=maxy);
+					lebende[x][y] -= 1;
+					/*lebende[x+1][y-1] += 1;
+					lebende[x+1][y] += 1;
+					lebende[x+1][y+1] += 1;
+					lebende[x][y-1] += 1;
+					lebende[x][y+1] += 1;
+					lebende[x-1][y-1] += 1;
+					lebende[x-1][y] += 1;
+					lebende[x-1][y+1] += 1;*/
+				}
+				x++;
+			} while(x<XMAX);
+			y++;
+		} while(y<YMAX);
+		clrscr ();
+		//for(y = 0; y< YMAX; y++){
+		y=0;
+		do {
+			x=0;
+			//for(x = 0; x< XMAX; x++){
+			do {
+				//gotoxy(x,y);
+				//cprintf("%d",lebende[x][y]);
+				switch (lebende[x][y])
+				{
+				case 2:
+					switch (array[x][y])
+					{
+					case 1:
+						temp[x][y] = 1;
+						break;
+					}
+					break;
+				case 3:
+					temp[x][y] = 1;
+					break;
+				}
+				switch (array[x][y])
+				{
+					case 1:
+						revers(1);
+						cputcxy (x, y, 32);
+						break;
+				}
+				//array[x][y] = temp[x][y];
+				x++;
+			} while(x < XMAX);
+			y++;
+		}	while(y < YMAX);
+		memcpy(array,temp,XMAX*YMAX);
+		round++;
+	}while(round < ROUNDS && !kbhit());
+		
+
+
+		t = clock() - t;
+	
+  /* Reset screen colors */
+    bgcolor (background);
+    textcolor (text);
+    clrscr ();
+
+    /* Calculate stats */
+    sec   = (t * 10) / CLK_TCK;
+    sec10 = sec % 10;
+    sec  /= 10;
+    fps   = (round * (CLK_TCK * 10)) / t;
+    fps10 = fps % 10;
+    fps  /= 10;
+
+    /* Output stats */
+    gotoxy (0, 0); cprintf ("time  : %lu.%us", sec, sec10);
+    gotoxy (0, 1); cprintf ("frames: %lu", round);
+    gotoxy (0, 2); cprintf ("fps   : %lu.%u", fps, fps10);
+
+    /* Wait for a key, then end */
+    cputsxy (0, 4, "Press any key when done...");
+    (void) cgetc ();
+
+    /* Done */
+    return EXIT_SUCCESS;
+}
